a088af76631f36bac2d8b23989d694c452009062
BankTHP <bang.225588z@gmail.com>
2023-09-28 14:37:06 +0700

    new result

--- src/main/java/com/pcc/portalservice/service/DepartmentService.java
+++ src/main/java/com/pcc/portalservice/service/DepartmentService.java
@@ -47,34 +47,59 @@ public class DepartmentService {
     }
 
     public List<Map<String, Object>> findAllJoinDepartments() {
-    String jpql = "SELECT c.company_name, s.sector_name, s.sector_code, d.dept_name, d.dept_code,p.id,p.position_name  FROM department d JOIN sector s ON d.sector_id = s.id JOIN company c ON s.company_id = c.id join position p on p.department_id = d.id ";
-
-    List<Object[]> results = entityManager.createNativeQuery(jpql).getResultList();
+        String jpql = "SELECT " +
+                "c.company_name, " +
+                "s.sector_name, " +
+                "s.sector_code, " +
+                "d.dept_name, " +
+                "d.dept_code, " +
+                "p.id, " +
+                "p.position_name " +
+                "FROM " +
+                "department d " +
+                "JOIN " +
+                "sector s ON d.sector_id = s.id " +
+                "JOIN " +
+                "company c ON s.company_id = c.id " +
+                "JOIN " +
+                "position p ON p.department_id = d.id;";
     
-    List<Map<String, Object>> resultList = new ArrayList<>();
+        List<Object[]> results = entityManager.createNativeQuery(jpql).getResultList();
     
-    for (Object[] row : results) {
-        Map<String, Object> resultMap = new LinkedHashMap<>();
-        resultMap.put("company", row[0]);
-        resultMap.put("sectorname", row[1]);
-        resultMap.put("sectorcode", row[2]);
-        resultMap.put("deptname", row[3]);
-        resultMap.put("deptcode", row[4]);
-
-        List<Map<String, String>> positionList = new ArrayList<>();
-        Map<String, String> positionMap = new LinkedHashMap<>();
-        
-        positionMap.put("id", row[5].toString());
-        positionMap.put("name", row[6].toString());
-        positionList.add(positionMap);
-
-        resultMap.put("position", positionList);
-
-        resultList.add(resultMap);
+        Map<String, Map<String, Object>> departmentMap = new HashMap<>();
+    
+        for (Object[] row : results) {
+            String departmentKey = row[4].toString();
+    
+            Map<String, Object> departmentInfo = departmentMap.computeIfAbsent(departmentKey, k -> {
+                Map<String, Object> deptData = new LinkedHashMap<>();
+                deptData.put("company", row[0]);
+                deptData.put("sectorname", row[1]);
+                deptData.put("sectorcode", row[2]);
+                deptData.put("deptname", row[3]);
+                deptData.put("deptcode", row[4]);
+                deptData.put("positions", new ArrayList<Map<String, String>>());
+                return deptData;
+            });
+    
+            Map<String, String> positionMap = new LinkedHashMap<>();
+            positionMap.put("id", row[5].toString());
+            positionMap.put("name", row[6].toString());
+    
+            List<Map<String, String>> positions = (List<Map<String, String>>) departmentInfo.get("positions");
+            positions.add(positionMap);
+        }
+    
+        return new ArrayList<>(departmentMap.values());
     }
-
-    return resultList;
-}
+    
+    
+    
+    
+    
+    
+    
+    
     public Optional<Department> findByDeptCodeAndDeptName(String DeptCode, String DeptName){
         return departmentRepository.findByDeptCodeAndDeptName(
                 DeptCode,DeptName);
