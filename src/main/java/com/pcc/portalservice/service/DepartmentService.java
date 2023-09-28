39cb2b74373dab3b2743c9e000a04f58bb35c410
BankTHP <bang.225588z@gmail.com>
2023-09-28 11:44:25 +0700

    update json Response body department

--- src/main/java/com/pcc/portalservice/service/DepartmentService.java
+++ src/main/java/com/pcc/portalservice/service/DepartmentService.java
@@ -47,8 +47,7 @@ public class DepartmentService {
     }
 
     public List<Map<String, Object>> findAllJoinDepartments() {
-    String jpql = "SELECT c.company_name, s.sector_name, s.sector_code, d.dept_name, d.dept_code FROM " + 
-         "department d JOIN sector s ON d.sector_id = s.id JOIN company c ON s.company_id = c.id";
+    String jpql = "SELECT c.company_name, s.sector_name, s.sector_code, d.dept_name, d.dept_code,p.id,p.position_name  FROM department d JOIN sector s ON d.sector_id = s.id JOIN company c ON s.company_id = c.id join position p on p.department_id = d.id ";
 
     List<Object[]> results = entityManager.createNativeQuery(jpql).getResultList();
     
@@ -61,6 +60,16 @@ public class DepartmentService {
         resultMap.put("sectorcode", row[2]);
         resultMap.put("deptname", row[3]);
         resultMap.put("deptcode", row[4]);
+
+        List<Map<String, String>> positionList = new ArrayList<>();
+        Map<String, String> positionMap = new LinkedHashMap<>();
+        
+        positionMap.put("id", row[5].toString());
+        positionMap.put("name", row[6].toString());
+        positionList.add(positionMap);
+
+        resultMap.put("position", positionList);
+
         resultList.add(resultMap);
     }
 
