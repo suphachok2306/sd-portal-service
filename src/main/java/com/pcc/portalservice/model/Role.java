e59e2f77e3d5dd61e3cfc0a6e50972b3170b2208
Suphachok <suphachok.h@ku.th>
2023-10-03 11:46:58 +0700

    setStatusToTraining

--- src/main/java/com/pcc/portalservice/model/Role.java
+++ src/main/java/com/pcc/portalservice/model/Role.java
@@ -6,8 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
+
 
 @Entity
 @Table(name = "ROLES")
