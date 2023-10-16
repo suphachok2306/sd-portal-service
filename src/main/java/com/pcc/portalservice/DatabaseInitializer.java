ffdf7a4fac651daa077045524781b4952661e3b1
LAPTOP-TB43LSM8\PPC-Bank <bang.225588z@gmail.com>
2023-10-16 14:09:01 +0700

    fix

--- src/main/java/com/pcc/portalservice/DatabaseInitializer.java
+++ src/main/java/com/pcc/portalservice/DatabaseInitializer.java
@@ -189,7 +189,7 @@ public class DatabaseInitializer implements CommandLineRunner {
     if (isTableEmpty("public.users")) {
       jdbcTemplate.execute(sql);
       jdbcTemplate.execute(
-        "INSERT INTO public.users_roles (user_id, roles_id) VALUES (1, 1),(2, 2),(3, 3),(3, 2),(4, 4),(5, 5),(6, 2),(7, 2),(8, 2),(9, 2),(10, 2)"
+        "INSERT INTO public.users_roles (user_id, roles_id) VALUES (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 2),(7, 2),(8, 2),(9, 2),(10, 2)"
       );
       System.out.println("Data inserted into public.users table.");
     } else {
